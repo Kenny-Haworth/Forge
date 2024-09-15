caa4fa5fa4e11a14cfc2fb6ad4e2456a2e91647b
Kenny Haworth <kennyhaworth2@gmail.com>
2024-09-14 21:22:38 -0700

    Added methods to delete a directory

--- forge/ForgeUtils.java
+++ forge/ForgeUtils.java
@@ -1,6 +1,13 @@
 package forge;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.TimerTask;
+import java.util.stream.Stream;
 
 /**
  * A collection of commonly used utility methods.
@@ -23,5 +30,53 @@ public final class ForgeUtils
                 runnable.run();
             }
         };
+    }
+
+    /**
+     * Deletes the given directory.
+     *
+     * @param directory The directory to delete
+     * @throws IOException If deleting the directory fails
+     */
+    public static void deleteDir(Path directory) throws IOException
+    {
+        try (Stream<Path> stream = Files.walk(directory))
+        {
+            stream.sorted(Comparator.reverseOrder())
+                  .map(Path::toFile)
+                  .forEach(File::delete);
+        }
+    }
+
+    /**
+     * Deletes the given directory.
+     *
+     * @param directory The directory to delete
+     * @throws IOException If deleting the directory fails
+     */
+    public static void deleteDir(File directory) throws IOException
+    {
+        try (Stream<Path> stream = Files.walk(directory.toPath()))
+        {
+            stream.sorted(Comparator.reverseOrder())
+                  .map(Path::toFile)
+                  .forEach(File::delete);
+        }
+    }
+
+    /**
+     * Deletes the given directory.
+     *
+     * @param directory The directory to delete
+     * @throws IOException If deleting the directory fails
+     */
+    public static void deleteDir(String directory) throws IOException
+    {
+        try (Stream<Path> stream = Files.walk(Paths.get(directory)))
+        {
+            stream.sorted(Comparator.reverseOrder())
+                  .map(Path::toFile)
+                  .forEach(File::delete);
+        }
     }
 }
