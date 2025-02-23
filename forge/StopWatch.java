02a1831eb0fb5521f429cdf9a40918af3ba0e203
Kenny Haworth <kennyhaworth2@gmail.com>
2025-02-22 19:46:18 -0800

    Added a method to StopWatch to print the estimated time remaining for processing a collection of elements

--- forge/StopWatch.java
+++ forge/StopWatch.java
@@ -34,4 +34,36 @@ public final class StopWatch
     {
         System.out.println("Time elapsed: " + ForgeUtils.timeToStr(System.nanoTime() - this.time));
     }
+
+    /**
+     * Prints the number of elements processed, percent complete, and estimated time remaining.
+     *
+     * @param elementsProcessed The number of elements processed thus far
+     * @param totalElements The total number of elements to process
+     * @param unit The unit of measurement for the elements (e.g. "MB"), or an empty String to not specify a unit
+     * @return True if the processing is complete, false otherwise
+     */
+    public boolean printEstimatedTimeRemaining(long elementsProcessed, long totalElements, String unit)
+    {
+        //if there are no elements to process, there is no time remaining
+        if (totalElements <= 0)
+        {
+            //note that "\033[K" deletes everything from the cursor to the end of line
+            System.out.print("\r\033[KProgress: 0/0 " + unit + ", 100.00%, Time remaining: Complete");
+            return true;
+        }
+
+        long timeElapsed = System.nanoTime() - this.time;
+        double percentComplete = (double)elementsProcessed / totalElements;
+
+        String strPercentComplete = String.format("%.2f%%", percentComplete * 100);
+        long nanoSecondsRemaining = (long)((timeElapsed / percentComplete) - timeElapsed);
+        String timeRemaining = ForgeUtils.timeToStr(nanoSecondsRemaining);
+
+        System.out.print("\r\033[KProgress: " + elementsProcessed + "/" + totalElements + " " + unit +
+                         ", " + strPercentComplete + ", Time remaining: " + (timeRemaining.isEmpty() ? "Complete" : timeRemaining));
+        System.out.flush();
+
+        return percentComplete >= 1;
+    }
 }
