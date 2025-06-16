730d110bb6ec8f5656a5db2714f39bb217d1adcc
Kenny Haworth <kennyhaworth2@gmail.com>
2025-06-16 10:38:29 -0700

    The build script can now be called from any dir

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-set -eu # exit on error or undefined variable
+#
+# Compiles the Forge into a single jar file.
+#
+# Usage: ./build.sh
+#
+# Output: Forge.jar (in bin)
 
-# compile the project
+set -eu # add x for debug
+
+# go to where this script lives
+cd $(dirname $0)
+
+# compile all files
 javac -d classes $(find forge -name "*.java")
 
-# package the jar
+# create the jar
 jar -cf bin/Forge.jar -C classes .
 
 # cleanup
 rm -rf classes
+echo "Forge compiled successfully"
